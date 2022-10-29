bfc6b3aa006e37609bbabcb562641a4df6542e59
Frederik Wulf <frede.w719@gmail.com>
2022-10-29 18:41:19 +0200

    fixed bug for next time in timer added sound on last 5 secs

--- src/app/pages/timer/timer.component.ts
+++ src/app/pages/timer/timer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Program } from '../../models/Program';
-import { mergeMap, Observable, tap } from 'rxjs';
+import { mergeMap, Observable, tap, timer } from 'rxjs';
 import { ProgramService } from '../../chore/services/program.service';
 import { Timer, TimerTypes } from '../../models/Timer';
 
@@ -11,6 +11,8 @@ import { Timer, TimerTypes } from '../../models/Timer';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
+
+  private myAudioContext = new AudioContext();
 
   remainingTime!: number;
 
@@ -56,7 +58,11 @@ export class TimerComponent implements OnInit {
     this.running = true;
       const startTime = Date.now() - (this.remainingTime * -1000);
       this.timerRef = setInterval(() => {
-        this.remainingTime = Math.round ((Date.now() - startTime) / -1000);
+        const calc = (Date.now() - startTime) / -1000
+        this.remainingTime = Math.round(calc);
+        if (this.remainingTime < 5 && calc % 1 === 0) {
+          this.playSound();
+        }
         if (this.remainingTime === 0) {
           if(this.setTimer(this.activeIndex+1)) {
             this.clearTimer();
@@ -82,5 +88,41 @@ export class TimerComponent implements OnInit {
       default:
         throw new Error('Invalid Type');
     }
+  }
+
+  playSound(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      // Set default duration if not provided
+      const duration = 50;
+      const frequency = 400;
+      const volume = 10;
+
+      try{
+        let oscillatorNode = this.myAudioContext.createOscillator();
+        let gainNode = this.myAudioContext.createGain();
+        oscillatorNode.connect(gainNode);
+
+        // Set the oscillator frequency in hertz
+        oscillatorNode.frequency.value = frequency;
+
+        // Set the type of oscillator
+        oscillatorNode.type= "square";
+        gainNode.connect(this.myAudioContext.destination);
+
+        // Set the gain to the volume
+        gainNode.gain.value = volume * 0.01;
+
+        // Start audio with the desired duration
+        oscillatorNode.start(this.myAudioContext.currentTime);
+        oscillatorNode.stop(this.myAudioContext.currentTime + duration * 0.001);
+
+        // Resolve the promise when the sound is finished
+        oscillatorNode.onended = () => {
+          resolve();
+        };
+      }catch(error){
+        reject(error);
+      }
+    });
   }
 }
